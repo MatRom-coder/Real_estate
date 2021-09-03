8b1c0f675f544e7a1638a8ae933e74d50b4cb414
Matteo <matrom01@gmail.com>
2021-09-03 17:42:33 +0200

    files added

---
+++ prediction_price.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+
+df = pd.read_csv("./Real_estate.csv")
