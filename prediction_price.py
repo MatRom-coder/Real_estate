2dff032580baaafc2491fe46e53eb7eb3e1c6d16
Matteo <matrom01@gmail.com>
2021-09-06 15:48:09 +0200

    added changes of prediction_price.py

--- prediction_price.py
+++ prediction_price.py
@@ -3,11 +3,13 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+
 
 df = pd.read_csv("./Real_estate.csv")
 
 
-x = df.drop(["No", "X1 transaction date", "X1 transaction date"], 1)
+x = df.drop(["No", "X1 transaction date", "X5 latitude", "X6 longitude", "Y house price of unit area"], 1)
 y = df["Y house price of unit area"]
 
 
@@ -17,6 +19,11 @@ model = LinearRegression()
 model.fit(x_train, Y_train)
 y_pred = model.predict(x_test)
 
+
+
+
 print(y_pred)
 print("MAE: ", mean_absolute_error(Y_test,y_pred))
+print("R^2: ", r2_score(Y_test,y_pred))
+
  
