407d8b6d70d694360a15b2a96c412e0404ff0e82
Matteo <matrom01@gmail.com>
2021-09-27 23:19:38 +0200

    all changes added

--- regression_algorithms/knn_regression.py
+++ regression_algorithms/knn_regression.py
@@ -1,72 +1,24 @@
 import pandas as pd
-# import numpy as np
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
-import matplotlib.pyplot as plt
+import joblib
 
 
+df = pd.read_csv("../Real_estate.csv")
+
+x = df.drop(["No", "X1 transaction date", "X5 latitude", "X6 longitude", "Y house price of unit area"], 1)
+y = df["Y house price of unit area"]
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size= 0.20)
+
+knn = KNeighborsRegressor(n_neighbors = 5)
+knn.fit(X_train, y_train)
+
+#Save the trained model to disk
+
+filename = '../trained_models/knn_model.sav'
+joblib.dump(knn, filename)
 
 
-class Knn:
-
-    def __init__(self):
-        
-        """
-         - Initializing Knn object with the number of neighbors setted to three
-         - Getting the data 
-        """
-        self.reg = KNeighborsRegressor(n_neighbors=3)
-        self.df = pd.read_csv('../Real_estate.csv')
-
-
-
-
-    def split_data(self):
-        
-        """
-        Splitting the data in training and test set with test size of 0.2.
-        """
-        
-        x = self.df.drop(["No", "X1 transaction date", "X5 latitude", "X6 longitude", "Y house price of unit area"], 1)
-        y = self.df["Y house price of unit area"]
-        x_train, x_test , Y_train, Y_test = train_test_split(x, y , test_size = 0.2)
-
-        return (x_train, x_test, Y_train, Y_test)
-
-
-    def training_model(self):
-        
-        """
-        training of the model
-        """
-        x_train = self.split_data()[0]
-        Y_train = self.split_data()[2]        
-        self.reg.fit(x_train, Y_train)
-
-    def prediction_accuracy(self):
-        
-        
-        """
-        The principal purpose of this function is to verify the accuracy of the model.
-
-        Returns:
-            float value: acc -> accuracy of the model obtained from the score function.
-            Score function include the predict function.
-        """
-        
-        x_test = self.split_data()[1]
-        Y_test = self.split_data()[3]
-        acc = 0  #let's initialize the accuracy value to 0
-        acc = self.reg.score(x_test,Y_test)
-
-        return acc
-
-        
-
-
-
-if __name__ == "__main__":
-    knn_obj = Knn()
-    knn_obj.split_data()
-    knn_obj.training_model()
-    print(knn_obj.prediction_accuracy())
+ 
